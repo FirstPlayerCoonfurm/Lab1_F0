7cc54c01e705fa8de77a32a339b47eae0b5f3c1f
FirstPlayerCoonfurm <119936322+FirstPlayerCoonfurm@users.noreply.github.com>
2023-09-16 14:22:58 +0700

    Update code

--- Lab1.py
+++ Lab1.py
@@ -11,59 +11,107 @@ logging.basicConfig(
 logging.getLogger().addHandler(logging.StreamHandler())
 
 
-def val_username(login: str):
-    try:
-        username = login
-        logging.info(f'Введён логин: {login}')
-        username_reg = re.compile(r"^\+?(\d[\d\-.\s]+)?(\([\d\-.\s]+\))?[\d\-.\s]+\d$|^([a-zA-Zа-яА-ЯЁё0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,})$|^([a-zA-Z0-9_]+)$")
+def val_username(login: str) -> (str, str):
+    while True:
+        try:
+            if not login:
+                logging.info("Логин не может быть пустым")
+                login = input("Введите логин: ")
+            
+            logging.info(f'Введён логин: {login}')
+            if len(login) == 5:
+                logging.info("Корректная длина логина")
 
-        if username_reg.match(username):
-            logging.info("username is correct!")
-            return True, "success"
+                username_reg = re.compile(
+                    r"^\+?(\d[\d\-.\s]+)?(\([\d\-.\s]+\))?[\d\-.\s]+\d$|^([a-zA-Zа-яА-ЯЁё0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,})$|^([a-zA-Z0-9_]+)$")
 
-    except Exception as err:
-        logging.error(err)
-        return False, "Неверный логин"
+                if re.match(r"^\+?(\d[\d\-.\s]+)?(\([\d\-.\s]+\))?[\d\-.\s]+\d$", login):
+                    logging.info("Имя пользователя верно!")
+                    return "True", "success"
+
+                if re.match(r"^([a-zA-Zа-яА-ЯЁё0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,})$", login):
+                    logging.info("Имя пользователя верно!")
+                    return "True", "success"
+
+                if re.match(r"^([a-zA-Z0-9_]+)$", login):
+                    logging.info("Имя пользователя верно!")
+                    return "True", "success"
+
+                logging.info("Неверный логин")
+                login = input("Неверный логин. Введите логин: ")
+            else:
+                logging.info("Длина логина неверна")
+                login = input("Неверная длина логина. Введите логин: ")
+
+        except Exception as err:
+            logging.error(err)
+            login = input("Ошибка ввода логина. Введите логин: ")
 
 
-def val_pass(password: str, conf_password: str):
-    try:
-        pass_user = password
-        logging.info(f'Введён пароль: {password}')
-        confirm_pass = conf_password
+def val_pass(password: str, conf_password: str) -> (str, str):
+    while True:
+        try:
+            logging.info(f'Введён пароль: {password}')
 
-        if pass_user == confirm_pass:
-            # Логирование успешной регистрации
-            logging.info('Пароль подтверждён, регистрация удачна')
-            return True, "Успешное подтверждение пароля"
+            if len(password) != 7:
+                logging.info("Неверная длина пароля")
+                password = input("Неверная длина пароля. Введите пароль: ")
+                continue
 
-        else:
-            # Логирование несовпадения паролей
-            logging.warning('Пароль не подтверждён, в регистрации отказано')
-            return False, "Пароли не совпадают!"
+            password_reg1 = re.compile(r"[a-zA-Z]+")
+            password_reg2 = re.compile(r"\d+")
 
-    except Exception as err:
-        logging.error(err)
-        return False, "Ошибка введённых паролей!"
+            if re.match(password_reg1, password) and re.match(password_reg2, password):
+                logging.info("Пароль верен!")
+
+                if password == conf_password:
+                    # Логирование успешной регистрации
+                    logging.info('Пароль подтверждён, регистрация удачна')
+                    return "True", "Успешное подтверждение пароля"
+
+                else:
+                    # Логирование несовпадения паролей
+                    logging.warning('Пароль не подтверждён, в регистрации отказано')
+                    password = input("Пароли не совпадают. Введите пароль: ")
+                    conf_password = input("Подтвердите пароль: ")
+                    continue
+            else:
+                logging.info("Неверный формат пароля")
+                password = input("Неверный формат пароля. Введите пароль: ")
+                continue
+
+        except Exception as err:
+            logging.error(err)
+            password = input("Ошибка введенного пароля. Введите пароль: ")
+            conf_password = input("Подтвердите пароль: ")
 
 
 def register(login: str, password: str, conf_password: str) -> (str, str):
     try:
         username_result, username_msg = val_username(login)
         pass_result, pass_msg = val_pass(password, conf_password)
-        
-        if username_result and pass_result:
-            return True, "Успешное выполнение"
+
+        if username_result == "True" and pass_result == "True":
+            return "True", "Успешное выполнение"
         else:
-            return False, "Ошибка валидации"
+            if username_result == "True":
+                return "False", pass_msg
+            else:
+                return "False", username_msg
 
-    except Exception as err:
-        logging.error(err)
-        return False, "Ошибка выполнения программы"
+
+    except:
+        logging.error()
+        return "False", "Ошибка выполнения программы"
 
 
-result = register(login=input("Введите логин: "), password=input("Введите пароль: "), conf_password=input("Подтвердите пароль: "))
-if result[0]:
-    print("Nice!")
+result = register(
+    login=input("Введите логин: "),
+    password=input("Введите пароль: "),
+    conf_password=input("Подтвердите пароль: ")
+)
+
+if result[0] == "True":
+    print("Nice!", result[1])
 else:
-    print("Error!")
+    print("Error!", result[1])
